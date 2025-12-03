2a81b52a4e98a9ff9ed4d3a006d493194db6bc8c
imadprogram <kimad7141@gmail.com>
2025-12-03 23:35:16 +0100

    improved form

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 const income_btn = document.getElementById('income-btn')
 const expense_btn = document.getElementById('expense-btn')
-const form_bg = document.querySelector('.income-form-bg')
+
+const income_form_bg = document.querySelector('.income-form-bg')
 const expense_form_bg = document.querySelector('.expense-form-bg')
-const form = document.querySelector('form')
+
+const income_form = income_form_bg.querySelector('form')
+const expense_form = expense_form_bg.querySelector('form')
 
 income_btn.addEventListener('click', () => {
-    form_bg.classList.remove('hidden')
-    form.classList.add('[animation:form-animation_.5s_ease]')
+    income_form_bg.classList.remove('hidden')
+    income_form.classList.add('[animation:form-animation_.5s_ease]')
 })
 expense_btn.addEventListener('click',()=>{
     expense_form_bg.classList.remove('hidden')
-    expense_form_bg.classList.add('[animation:form-animation_.5s_ease]')
+    expense_form.classList.add('[animation:form-animation_.5s_ease]')
 })
-form_bg.addEventListener('click', () => {
-    form.classList.remove('[animation:form-animation_.5s_ease]')
-    form.classList.add('[animation:form-animation_.5s_ease_reverse]')
-    setTimeout(() => {
-        form_bg.classList.add('hidden')
-    }, 500);
+
+
+income_form_bg.addEventListener('click', () => {
+    income_form.classList.remove('[animation:form-animation_.5s_ease]')
+    income_form_bg.classList.add('hidden')
 })
-form.addEventListener('click', (e) => {
-    e.stopPropagation()
+expense_form_bg.addEventListener('click', () => {
+    expense_form.classList.remove('[animation:form-animation_.5s_ease]')
+    expense_form_bg.classList.add('hidden')
 })
+
+
+income_form.addEventListener('click', (e) => {e.stopPropagation()})  
+expense_form.addEventListener('click', (e) => {e.stopPropagation()})  
